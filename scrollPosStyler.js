fe7ad10caecf7cccac684e6074ab2504fbe33360
Achim <acch@users.noreply.github.com>
2015-10-11 19:56:40 +0200

    Added code header with version, license and copyright

--- scrollPosStyler.js
+++ scrollPosStyler.js
@@ -1,3 +1,11 @@
+/* ========================================================================
+ * ScrollPos-Styler v0.1
+ * https://github.com/acch/scrollpos-styler
+ * ========================================================================
+ * Copyright 2015 Achim Christ
+ * Licensed under MIT (https://github.com/acch/scrollpos-styler/blob/master/LICENSE)
+ * ======================================================================== */
+
 "use strict";
 
 var scrollPosY = 0,
